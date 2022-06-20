0a66246959cbf1c2466653cafd4529618f66cd09
Gilly <gilly.huga@yahoo.co.id>
2022-06-20 07:59:10 +0700

    feat: add chakra UI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+
+function Home() {
+
+    return (
+        <div>
+            <Heading>To Do List</Heading>
+        </div>
+    );
+}
+
+export default Home;
