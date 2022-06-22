5a43e6be94d2060872b53cd2c18a151693091a60
Gilly <gilly.huga@yahoo.co.id>
2022-06-22 21:23:35 +0700

    feat: add slicing home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,66 @@
-import { Heading } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer, Container,
+  Heading,
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { EditIcon, DeleteIcon, CheckIcon } from "@chakra-ui/icons";
+
+const data = [
+  {
+    title: 'Makan Siang',
+    isComplete: true
+  },
+  {
+    title: 'Makan Malam',
+    isComplete: true
+  },
+  {
+    title: 'Mandi',
+    isComplete: false
+  },
+]
 
 function Home() {
-
-    return (
-        <div>
-            <Heading>To Do List</Heading>
-        </div>
-    );
+  return (
+    <Container p={5} maxW='container.xl'>
+      <Heading py={10}>To Do List</Heading>
+      <TableContainer>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Title</Th>
+              <Th isNumeric>Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((item) => (
+              <Tr>
+                <Td>{item.title}</Td>
+                <Td isNumeric>
+                  <ButtonGroup
+                    variant="solid"
+                    spacing="4"
+                    size="md"
+                  >
+                    <Button leftIcon={<CheckIcon />} colorScheme="teal">Complete</Button>
+                    <Button leftIcon={<EditIcon />} colorScheme="yellow">Update</Button>
+                    <Button leftIcon={<DeleteIcon />} colorScheme="red">Delete</Button>
+                  </ButtonGroup>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default Home;
